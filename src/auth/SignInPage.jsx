e2b3678af7c47b10eb50db5754b19fe27114aa4f
Shwetaank <spmorey87@gmail.com>
2024-08-12 00:18:37 +0530

    reorganized sign in functnality

--- src/auth/SignInPage.jsx
+++ src/auth/SignInPage.jsx
@@ -1,11 +1,44 @@
-import { SignIn } from "@clerk/clerk-react"
+import { SignIn } from "@clerk/clerk-react";
+import { FaGithub } from "react-icons/fa";
 
 const SignInPage = () => {
   return (
-    <div className=" flex justify-center items-center ">
-      <SignIn/>
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        <SignIn
+          appearance={{
+            elements: {
+              card: "bg-transparent shadow-none",
+              formButtonPrimary:
+                "w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300",
+              inputField:
+                "border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 py-2 px-4 mb-4",
+              headerTitle: "text-2xl font-semibold text-center mb-4",
+              headerSubtitle:
+                "text-center text-sm text-gray-500 bg-transparent mb-6",
+              footer: "text-center text-gray-500 text-sm mt-6",
+            },
+            layout: {
+              logoPlacement: "none",
+              headerPlacement: "inside",
+              headerContent: "AlmaBetter Quiz App",
+            },
+          }}
+        />
+        <div className="mt-6 sm:mt-8 text-center">
+          <a
+            href="https://github.com/Shwetaank/almabetter-quizz-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center justify-center font-semibold border border-gray-300 py-2 px-4 rounded-md shadow-lg hover:text-blue-600 transition-colors duration-300"
+          >
+            <span> Give Star Me on </span>
+            <FaGithub className="text-4xl ml-2" />
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignInPage
+export default SignInPage;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,32 @@
 // import { UserButton } from "@clerk/clerk-react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      Landing Screen
+      <div className="flex flex-col items-center justify-center min-h-screen text-center bg-gradient-to-b from-blue-100 to-red-100">
+        <h1 className="text-4xl font-bold mb-4 text-white">
+          Welcome to Quiz Platform
+        </h1>
+        <p className="text-lg text-white mb-6">
+          Your go-to place for fun and challenging quizzes!
+        </p>
+        <div className="flex gap-4">
+          <Link to="/play-quiz">
+            <button className="bg-black text-white px-6 py-2 rounded-md font-semibold hover:bg-gray-800">
+              Play Quiz
+            </button>
+          </Link>
+          <Link to="/create-quiz">
+            <button className="bg-green-500 text-white px-6 py-2 rounded-md font-semibold hover:bg-green-600">
+              Create Quiz
+            </button>
+          </Link>
+        </div>
+      </div>
       <Footer />
     </div>
   );
